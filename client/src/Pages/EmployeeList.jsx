3c63ed89fb3484baa38922f2aa9775094689735b
gavaller-vid <gavalleratilla@gmail.com>
2023-06-09 15:05:03 +0200

    change attendance thick on client side after check

--- client/src/Pages/EmployeeList.jsx
+++ client/src/Pages/EmployeeList.jsx
@@ -24,7 +24,12 @@ const EmployeeList = () => {
   const [missingShowed, setMissingShowed] = useState(false);
   const [currentPage, setCurrentPage] = useState(1);
   const [employeesPerPage, setEmployeesPerPage] = useState(10);
-    
+  const [attendanceChanged, setAttendanceChanged] = useState(1)
+
+  const lastEmployeeIndex = currentPage * employeesPerPage;
+  const firstEmployeeIndex = lastEmployeeIndex - employeesPerPage
+  //const currentEmployees = employees.slice(firstEmployeeIndex, lastEmployeeIndex)
+
   const fetchEmployeesOnCancel = () => {
     return fetch("/api/employees").then((res) => res.json()).then((employees) => {
       setLoading(false);
@@ -46,14 +51,14 @@ const EmployeeList = () => {
     const missingEmployees = employees.filter((employee) => employee.attendance === 'missing')
     setEmployees(missingEmployees)
   }
-
+  
   const handleSort = (key) => {
     const actualDirection = sorted === 'ascending' ? 'descending' : 'ascending'
     const newEmployees = sort(employees, key, actualDirection);
     setSorted(actualDirection);
     setEmployees(newEmployees);
   }
-
+  
   const sort = (list, key, direction) => {
     const ascendingEmps = [...list].sort((a,b) => {
       return direction === 'ascending' 
@@ -62,37 +67,37 @@ const EmployeeList = () => {
     })
     return ascendingEmps
   }
-
+  
   const sortLastAsc = () => {
     const ascLastNamesEmps = [...employees].sort((a,b) => {
       const lastNameA = a.name.split(' ').slice(-1)[0];
       const lastNameB = b.name.split(' ').slice(-1)[0];
-
+      
       return lastNameA.localeCompare(lastNameB);
     })
     setEmployees(ascLastNamesEmps)
   };
-
+  
   const sortLastDec = () => {
     const descLastNamesEmps = [...employees].sort((a,b) => {
       const lastNameA = a.name.split(' ').slice(-1)[0];
       const lastNameB = b.name.split(' ').slice(-1)[0];
-
+      
       return lastNameB.localeCompare(lastNameA);
     })
     setEmployees(descLastNamesEmps)
   }
-
+  
   const sortMiddleAsc = () => {
     const ascMiddleNamesEmps = [...employees].sort((a,b) => {
       const middleNameA = getMiddleName(a.name)
       const middleNameB = getMiddleName(b.name)
-
+      
       return middleNameA.localeCompare(middleNameB);
     })
     setEmployees(ascMiddleNamesEmps)
   };
-
+  
   const sortMiddleDec = () => {
     const descMiddleNamesEmps = [...employees].sort((a,b) => {
       const middleNameA = getMiddleName(a.name)
@@ -102,7 +107,7 @@ const EmployeeList = () => {
     })
     setEmployees(descMiddleNamesEmps)
   }
-
+  
   const sortMiddleName = () => {
     if (sortedByMiddle) {
       setsortedByMiddle(false)
@@ -112,7 +117,7 @@ const EmployeeList = () => {
       sortMiddleDec()
     }
   }
-
+  
   const sortByLastN = () => {
     if(sortedByLast) {
       setsortedByLast(false)
@@ -122,25 +127,25 @@ const EmployeeList = () => {
       sortLastDec()
     }
   }
-
+  
   const getMiddleName = (name) => {
     const nameParts = name.split(' ');
-
+    
     if(nameParts.length > 2) {
       return nameParts[1]
     }
-
+    
     return '';
   }
-
+  
   const fetchSearchedEmployee = (searchParam) => {
     const caseFormattedParam = searchParam.charAt(0).toUpperCase() + searchParam.slice(1).toLowerCase();
-
-        return fetch(`/api/employees/${caseFormattedParam}`).then((res) => res.json()).then((employees) => {
-            setEmployees(employees)
-        })
+    
+    return fetch(`/api/employees/${caseFormattedParam}`).then((res) => res.json()).then((employees) => {
+      setEmployees(employees)
+    })
   }
-
+  
   const setEmployeeAttendanceOnCheck = (attendanceId, attendanceState) => {
     return fetch(`/api/employees/${attendanceId}`, {
       method: "PATCH",
@@ -148,17 +153,26 @@ const EmployeeList = () => {
         "Content-Type": "application/json",
       },
       body: JSON.stringify(attendanceState),
-    }).then((res) => res.json())
+    }).then((res) => {
+      res.json()
+      if (attendanceChanged === 1) {
+        setAttendanceChanged(2)
+      } else {
+        setAttendanceChanged(1)
+      }
+    })
   }
-
+  
   useEffect(() => {
     fetchEmployees()
-      .then((employees) => {
-        setLoading(false);
-        setEmployees(employees);
-      })
-  }, []);
+    .then((employees) => {
+      setLoading(false);
+      setEmployees(employees);
+    })
+  }, [attendanceChanged]);
 
+
+  
   if (loading) {
     return <Loading />;
   }
@@ -176,7 +190,7 @@ const EmployeeList = () => {
     onSortMiddleName={sortMiddleName} 
     setSearchClicked={setSearchClicked}
     onCheck={setEmployeeAttendanceOnCheck}
-  />
+    />
   </>)
 };
 
